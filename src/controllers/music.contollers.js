53af317acedc255edd2e489e3aa322fdd2da3f6c
Nitin <rajnitin793@gmail.com>
2026-07-03 17:40:28 +0530

    created addAlbum api

--- src/controllers/music.contollers.js
+++ src/controllers/music.contollers.js
@@ -2,6 +2,7 @@ const musicModel = require('../models/music.model')
 const userModel = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 const {filesUpload} = require('../services/storage.service')
+const albumModel = require('../models/album.model')
 
 async function createMusic(req, res) {
     try {
@@ -45,5 +46,44 @@ async function createMusic(req, res) {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+
+async function createAlbum(req,res) {
+    try {
+       
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+                message: "unauthorized"
+            })
+    }
+    
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    if(decoded.role !== 'artist'){
+        return res.status(403).json({
+                message: "You don't have access to create an music"
+            })
+    }
+
+    const {title, musicIds} = req.body;
+
+    const album = await albumModel.create({
+        title,
+        musics: musicIds,
+        artist: decoded.id
+    })
+
+    res.status(201).json({
+        message: "new album created",
+        album: album
+    })
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
 
 module.exports = { createMusic }
